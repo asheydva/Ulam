ca5332d28400bc3a5b5a65c7088bfa1d1f04ad06
Oleg Sheydvasser <oleg.sheydvasser@autodesk.com>
2019-07-06 19:43:10 -0400

    runs

--- UlamSecCPP/UlamSecCPP/UlamSecCPP.cpp
+++ UlamSecCPP/UlamSecCPP/UlamSecCPP.cpp
@@ -3,12 +3,76 @@
 
 #include "pch.h"
 #include <iostream>
-#include<string> // for string class 
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>    // std::min_element, std::max_element
+
 using namespace std;
 
-string ulam_sequence(int n, int X)
+int min_element(unordered_set<int> set)
 {
-    return "not implemented\n";
+    // quick and dirty
+    int curMit = INT_MAX;
+    for (int elem : set)
+        if (elem < curMit)
+            curMit = elem;
+    return curMit;
+}
+
+// Adds/removes the element. Returns True if removes, False otherwise
+bool removeOrAdd(unordered_set<int> &set, int elem)
+{
+    // try to remove
+    if (set.erase(elem))
+        return true;
+    // otherwise insert
+    set.insert(elem);
+    return false;
+}
+
+int ulam_sequence(int n, int X)
+{
+    vector<int> ulam_seq{ 1, n };
+    unordered_set<int> unique_set{ n + 1 };
+    unordered_set<int> non_unique_set;
+
+    int largest_elem = n + 1;
+    
+    cout << "1\n" << n << '\n';
+
+    while (true)
+    {
+        int smallest_unique = min_element(unique_set);
+
+        for (int elem : ulam_seq)
+        {
+            int u = smallest_unique + elem;
+
+            // Look in non_unique_set to see if u is in there
+            if (!non_unique_set.count(u))
+            {
+                if (removeOrAdd(unique_set, u))
+                {
+                    // If already in unique_set, add to non_unique_set
+                    non_unique_set.insert(u);
+                }
+            }
+
+        }
+
+        smallest_unique = min_element(unique_set);
+        unique_set.erase(smallest_unique);
+
+        int largest_elem = smallest_unique;
+        if (largest_elem > X)
+            break;
+
+        ulam_seq.push_back(largest_elem);
+        cout << largest_elem << '\n';
+    }
+
+    return ulam_seq.size();
 }
 
 int main(int argc, char *argv[])
@@ -20,6 +84,6 @@ int main(int argc, char *argv[])
     int X = 1000;
     string fileName = "";
 
-    cout << "ulam_sequence(" << n << "," << X << ")\n" << ulam_sequence(n, X);
+    int nMembers = ulam_sequence(n, X);
+    cout << "ulam_sequence(" << n << "," << X << ") has " << nMembers << " members\n";
 }
-
