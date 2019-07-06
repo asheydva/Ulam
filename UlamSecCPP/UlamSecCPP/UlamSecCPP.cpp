dff23e070733aafb492300e1c363b0635b9de8ed
Oleg Sheydvasser <oleg.sheydvasser@autodesk.com>
2019-07-06 18:19:51 -0400

    set up call

--- UlamSecCPP/UlamSecCPP/UlamSecCPP.cpp
+++ UlamSecCPP/UlamSecCPP/UlamSecCPP.cpp
@@ -3,8 +3,23 @@
 
 #include "pch.h"
 #include <iostream>
+#include<string> // for string class 
+using namespace std;
 
-int main()
+string ulam_sequence(int n, int X)
 {
-    std::cout << "Hello World!\n"; 
+    return "not implemented\n";
 }
+
+int main(int argc, char *argv[])
+{
+    for (int count = 0; count < argc; ++count)
+        cout << "command line arguments: " << count << " " << argv[count] << '\n';
+
+    int n = 2;
+    int X = 1000;
+    string fileName = "";
+
+    cout << "ulam_sequence(" << n << "," << X << ")\n" << ulam_sequence(n, X);
+}
+
